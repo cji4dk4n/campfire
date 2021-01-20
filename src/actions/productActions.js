7ed9509ba4f4fbcb8db96ca9dacca01a7f4507d0
cji4dk4n <kansin0759@gmail.com>
2021-01-20 11:43:25 +0800

    弄config

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,8 +1,13 @@
 import { LOADING } from '../constants/types'
-import products from '../apis/products'
+// import products from '../apis/products'
+
+import { products } from '../db/db.json'
+
+const fakeFeach = () => new Promise(resolve => resolve({ data: products }))
 
 export const fetchProducts = () => async dispatch => {
     dispatch({ type: LOADING, payload: true})
-    const resData = await products.get('/products').catch(err => 'server is down')
+    // const resData = await products.get('/products').catch(err => 'server is down')
+    const resData = await fakeFeach().catch(err => 'server is down')
     return { resData, dispatch }
 }
