3d5895adeb10c79657f754ee9d57efb22510f864
cji4dk4n <kansin0759@gmail.com>
2021-01-20 11:24:37 +0800

    Updates

---
+++ precache-manifest.8dfc456f1ff501999dd5cf89f53bdf72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7ac36980800f76a2e3",
+    "url": "/static/css/main.6c6f9d93.chunk.css"
+  },
+  {
+    "revision": "3d7ac36980800f76a2e3",
+    "url": "/static/js/main.0c58254e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9c156bf64527b068b794",
+    "url": "/static/js/2.77fe283f.chunk.js"
+  },
+  {
+    "revision": "1b79d57fe7241960984689d67d3b9492",
+    "url": "/static/media/product-loading.1b79d57f.gif"
+  },
+  {
+    "revision": "5a399b1d151f0a8d07a554b026e2cb4c",
+    "url": "/static/media/search-icon.5a399b1d.gif"
+  },
+  {
+    "revision": "b6c8c3dea92acd187377b4f884787d9f",
+    "url": "/static/media/star.b6c8c3de.svg"
+  },
+  {
+    "revision": "4a4131f4e9806227cde6027144e24cc3",
+    "url": "/index.html"
+  }
+];
